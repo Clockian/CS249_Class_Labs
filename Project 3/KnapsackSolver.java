31244bfb04fcc91db5642f4c988dd5932494a700
Clockian <jrsaydyk@gmail.com>
2017-04-10 02:07:11 -0700

    F*** my life, saving progress, I give up

--- Project 3/KnapsackSolver.java
+++ Project 3/KnapsackSolver.java
@@ -1,6 +1,5 @@
-
 public class KnapsackSolver implements IKnapsackSolver{
-	int n = 0;
+	
 	@Override
 	public boolean[] knapsack(double capacity, Item[] items) {
 		boolean[] prior = new boolean[items.length];
@@ -13,75 +12,39 @@ public class KnapsackSolver implements IKnapsackSolver{
 	@Override
 	public boolean[] knapsackRecursive(double capacity, Item[] items, boolean[] prior) {
 		return knapsackRecursive2(capacity, 0, items, prior);
-		// Base Case
-		  /*
-		   if (n == 0 || capacity <= 0)
-		       return 0;
-
-		   // If weight of the nth item is more than Knapsack capacity W, then
-		   // this item cannot be included in the optimal solution
-		   if (wt[n-1] > W)
-		       return knapSack(W, wt, val, n-1);
-
-		   // Return the maximum of two cases: (1) nth item included (2) not included
-		   else return max( val[n-1] + knapSack(W-wt[n-1], wt, val, n-1),
-		                    knapSack(W, wt, val, n-1)
-		                  );*/
 	}
 	
 	public boolean[] knapsackRecursive2(double capacity, int n, Item[] items, boolean[] prior) {
-		
+		// Checks if it exceeds capacity or array size
 		if(capacity <= 0 || n == items.length){
-			prior[n] = true;
 			return prior;
 		}
 		
-		// skip if exceed capacity
+		// Skip if exceed capacity
 		if(items[n].getWeight() > capacity){
 			return knapsackRecursive2(capacity, n + 1, items, prior);
 		}
 		
 		else{
-			return knapsackRecursive2(capacity - items[n].getWeight(), n + 1, items, prior);
-		}
-		
-		/*
-		if(n != 0 && items[n].getValue() <= items[n - 1].getValue()){
-			prior[n] = false;
-			return knapsackRecursive2(capacity, n + 1, items, prior);
-		}
-		 */
-	}
-	
-	public boolean[] knapsackRecursive3(double capacity, int n, Item[] items, boolean[] prior) {
-		frownyface(capacity, n, items, prior);
-		return prior;
-	}
-	
-	public double frownyface(double capacity, int n, Item[] items, boolean[] prior){
-		if(capacity <= 0 || n == items.length){
-			return 0;
-		}
-		
-		if(items[n].getWeight() > capacity){
-			return frownyface(capacity, n + 1, items, prior);
-		}
-		
-		else{
+			boolean[] prior1 = knapsackRecursive2(capacity, n + 1, items, prior);		
+			boolean[] prior2 = knapsackRecursive2(capacity - items[n].getWeight(), n + 1, items, prior);
+			//prior[n] = true;
 			
-			//return frownyface(capacity, n + 1, items, prior[n] = false).compareTo(frownyface(capacity - items[n].getWeight(), n + 1, items, prior[n] = true) + items[n].getValue());
-			
-			double total1 = frownyface(capacity, n + 1, items, prior);
-			double total2 = frownyface(capacity - items[n].getWeight(), n + 1, items, prior) + items[n].getValue();
-			
-			if(total1 > total2){
-				prior[n] = true;
-				return total1;
+			double val1 = 0;
+			double val2 = 0;
+			for(int ii = 0; ii < prior1.length; ii++){
+				if(prior1[ii] == true){
+					val1 = val1 + items[ii].getValue();
+				}
+				if(prior2[ii] == true){
+					val2 = val2 + items[ii].getValue();
+				}
+			}
+			if(val1 >= val2){
+				return prior1;
 			}
 			else{
-				prior[n] = true;
-				//prior[n] = false;
-				return total2;
+				return prior2;
 			}
 		}
 	}
--- Project 3/NQueensSolver.java
+++ Project 3/NQueensSolver.java
@@ -1,9 +1,108 @@
-
 public class NQueensSolver implements INQueensSolver{
+
+	/*private int N;
 	
 	@Override
 	public boolean[][] nQueens(int n) {
+		N = n;
+		boolean[][] prior = new boolean[N][N];
+		return nQueensRecursive(n, prior, 0, 0);
+	}
+
+	@Override
+	public boolean[][] nQueensRecursive(int n, boolean[][] prior, int i, int j) {
+		int board[][] = new int[N][N];
+		for(int ii = 0; ii < prior.length; ii++){
+			for(int jj = 0; jj < prior.length; jj++){
+				board[ii][jj] = 0;
+			}
+		}
+		
+		if (solveNQUtil(board, 0) == false)
+        {
+            System.out.print("Solution does not exist");
+            return null;
+        }
+		
+		for(int ii = 0; ii < prior.length; ii++){
+			for(int jj = 0; jj < prior.length; jj++){
+				if(board[ii][jj] == 0){
+					prior[ii][jj] = false;
+				}
+				else{
+					prior[ii][jj] = true;
+				}
+			}
+		}
+        //printSolution(board);
+        return prior;
+	}
+	
+		// A recursive utility function to solve N
+	    //Queen problem
+	 public boolean solveNQUtil(int board[][], int col)
+	 {
+	     // base case: If all queens are placed
+	     // then return true
+	     if (col >= N)
+	         return true;
+	
+	     // Consider this column and try placing
+	     // this queen in all rows one by one
+	     for (int i = 0; i < N; i++)
+	     {
+	         // Check if queen can be placed on
+	         // board[i][col]
+	         if (isSafe(board, i, col))
+	         {
+	             // Place this queen in board[i][col]
+	             board[i][col] = 1;
+	
+	             // recur to place rest of the queens
+	             if (solveNQUtil(board, col + 1) == true)
+	                 return true;
+	
+	             // If placing queen in board[i][col]
+	             // doesn't lead to a solution then
+	             // remove queen from board[i][col]
+	             board[i][col] = 0; // BACKTRACK
+	         }
+	     }
+	
+	     // If queen can not be place in any row in
+	     // this colum col, then return false
+	     return false;
+	 }
+	 
+	 public boolean isSafe(int board[][], int row, int col)
+	    {
+	        int i, j;
+	 
+	        // Check this row on left side
+	        for (i = 0; i < col; i++)
+	            if (board[row][i] == 1)
+	                return false;
+	 
+	        // Check upper diagonal on left side
+	        for (i=row, j=col; i>=0 && j>=0; i--, j--)
+	            if (board[i][j] == 1)
+	                return false;
+	 
+	        // Check lower diagonal on left side
+	        for (i=row, j=col; j>=0 && i<N; i++, j--)
+	            if (board[i][j] == 1)
+	                return false;
+	 
+	        return true;
+	    }*/
+	@Override
+	public boolean[][] nQueens(int n) {
 		boolean[][] prior = new boolean[n][n];
+		for(int ii = 0; ii < prior.length; ii++){
+			for(int jj = 0; jj < prior.length; jj++){
+				prior[ii][jj] = false;
+			}
+		}
 		return nQueensRecursive(n, prior, 0, 0);
 	}
 
@@ -23,23 +122,28 @@ public class NQueensSolver implements INQueensSolver{
 		if(curQueens == numQueens){
 			return true;
 		}
-		
-		// Check if Queen can be placed on board (row, col)
-		if(check(numQueens, prior, row, col)){
-			// If so, place Queen
-			prior[row][col] = true;
-			curQueens++;
+		//for(int icol = 0; icol < prior.length; icol++){
+			// Check if Queen can be placed on board (row, col)
+			if(check(prior, row, col++)){
+				// If so, place Queen
+				prior[row][col] = true;
+				curQueens++;
+				
+				// Recur to place rest of Queens
+				if(solveNQueens(numQueens, curQueens, prior, row++, 0)){
+					return true;
+				}
 			
-			// Recur to place rest of Queens
-			if(solveNQueens(numQueens, curQueens, prior, row, col)){
-				return true;
-			}
-		}
-		
+				// If placing queen in board doesn't lead to a solution then remove queen from board
+		        prior[row][col] = false;
+		        curQueens--;
+				}
+		//}
+		// If queen can not be place in any row in this col, then return false
 		return false;
 	}
 	
-	public boolean check(int numQueens, boolean[][] prior, int row, int col){
+	public boolean check(boolean[][] prior, int row, int col){
 		// Check row for any Queens
 		for(int irow = 0; irow < prior.length; irow++){
 			if(prior[irow][col] == true){
@@ -69,14 +173,14 @@ public class NQueensSolver implements INQueensSolver{
 		}
 		
 		// Check upper diagonal on right side
-		for(int krow = row, kcol = col; krow >= 0 && kcol < prior.length; krow++, kcol--){
+		for(int krow = row, kcol = col; krow >= 0 && kcol < prior.length; krow--, kcol++){
             if(prior[krow][kcol] == true){
                 return false;
             }
 		}
 		
         // Check lower diagonal on left side
-		for(int krow = row, kcol = col; krow < prior.length && kcol >= 0; krow--, kcol++){
+		for(int krow = row, kcol = col; krow < prior.length && kcol >= 0; krow++, kcol--){
             if(prior[krow][kcol] == true){
                 return false;
             }
