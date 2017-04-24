800825b381fe577bb1ea8e8c5426fe85c470cda1
Jasque Saydyk <jrsaydyk@gmail.com>
2017-04-24 04:14:10 -0700

    Almost Done

--- Project 4/RedBlackTree.java
+++ Project 4/RedBlackTree.java
@@ -1,4 +1,4 @@
-import BinarySearchTree.Node;
+import java.util.NoSuchElementException;
 
 public class RedBlackTree<K extends Comparable<K>, E> implements IBinarySearchTree<K, E>{
 
@@ -14,6 +14,124 @@ public class RedBlackTree<K extends Comparable<K>, E> implements IBinarySearchTr
 		size++;
 	}
 	
+	/*private void checkPut(Node<K, E> node){
+		while(node.key.compareTo(root.key) != 0 && node.parent.color == false){
+			// There is a Red Red violation
+			if(node.parent.key.compareTo(node.parent.parent.left.key) == 0){
+				// Check if Uncle is Red
+				if(node.parent.parent.right == null || node.parent.parent.right.color == true){
+					// Uncle is black
+					if(node.key.compareTo(node.parent.right.key) == 0){
+						// make addingNode a left child
+						node = node.parent;
+						rotateLeft(node);
+					}
+					// recolor and rotate
+					node.parent.color = true;
+					node.parent.parent.color = false;
+					rotateRight(node.parent.parent);
+					
+				}
+				else{
+					node.parent.color = true;
+					node.parent.parent.right.color = true;
+					node.parent.parent.color = false;
+					node = node.parent.parent;
+				}
+			}
+			else{
+				// mirror image of above code on other side of the tree
+				if(node.parent.parent.left.color == false){
+					// Uncle is red
+					node.parent.color = true;
+					node.parent.parent.left.color = true;
+					node.parent.parent.color = false;
+					node = node.parent.parent;
+				}
+				else{
+					// Uncle is black
+					if(node.key.compareTo(node.parent.left.key) == 0){
+						node = node.parent;
+						rotateRight(node);
+					}
+					node.parent.color = true;
+					node.parent.parent.color = false;
+					rotateLeft(node.parent.parent);
+				}
+			}
+		}
+		// out of loop, last thing to do
+		root.color = true;
+	}
+	
+	private void rotateLeft(Node<K, E> node){
+		Node<K, E> nodeRight = node.right;
+		
+		// Establish right link
+		node.right = nodeRight.left;
+		if(nodeRight.left != null){
+			nodeRight.parent = node;
+		}
+		
+		// Establish right parent link
+		if(nodeRight != null){
+			nodeRight.parent = node.parent;
+		}
+		if(node.parent != null){
+			if(node.key.compareTo(node.parent.left.key) == 0){
+				node.parent.left = nodeRight;
+			}
+			else{
+				node.parent.right = nodeRight;
+			}
+		}
+		else{
+			root = nodeRight;
+		}
+		
+		// link x and y
+		nodeRight.left = node;
+		if(node != null){
+			node.parent = nodeRight;
+		}
+	}
+	
+	private void rotateRight(Node<K, E> node){
+		
+		// establish left link
+		Node<K, E> nodeLeft = node.left;
+		System.out.println("root.key:" + root.key);
+		System.out.println("node.key:" + node.key);
+		System.out.println("nodeLeft:" + nodeLeft);
+		System.out.println("node.left:" + node.left.key);
+		node.left = nodeLeft.right;
+		if(nodeLeft.right != null){
+			nodeLeft.right.parent = node;
+		}
+		
+		// Establish left parent link
+		if(nodeLeft != null){
+			nodeLeft.parent = node.parent;
+		}
+		if(node.parent != null){
+			if(node.key.compareTo(node.parent.right.key) == 0){
+				node.parent.right = nodeLeft;
+			}
+			else{
+				node.parent.left = nodeLeft;
+			}
+		}
+		else{
+			root = nodeLeft;
+		}
+		
+		// Link node and left
+		nodeLeft.right = node;
+		if(node != null){
+			node.parent = nodeLeft;
+		}
+	}*/
+	
 	/**
 	 * Adds a node to the tree containing this key element combination.
 	 * Must follow the four rules of a Red Black tree
@@ -27,146 +145,480 @@ public class RedBlackTree<K extends Comparable<K>, E> implements IBinarySearchTr
 	 */
 	@Override
 	public void put(K key, E element) {
-		// First, add node to tree and make it Red
-		Node<K, E> addingNode = new Node<>(false, key, element);
-		Node<K, E> currentNode = root;
-		int putHeight = this.placeInitialRedValue(key, element);
-		
-		checkPut(putHeight, addingNode);
-		
-		
-		
+		// PlaceInitialRedValue should return node created
+		Node<K, E> node = placeInitialRedValue(key, element);
+		if(node == null){
+			return;
+		}
+		else{
+			checkPutCase1(node);
+		}
 	}
 	
-	private boolean checkPut(int putHeight, Node<K, E> addingNode){
-		// Check if node is root, if so, make it Black, then done
-		if(root.color == false){
+	private Node<K, E> getGrandParent(Node<K, E> node){
+		if(node != null && node.parent != null){
+			return node.parent.parent;
+		}
+		else{
+			return null;
+		}
+	}
+	
+	private Node<K, E> getUncle(Node<K, E> node){
+		Node<K, E> grandParent = getGrandParent(node);
+		if(grandParent == null){
+			return null;
+		}
+		if(node.parent == grandParent.left){
+			return grandParent.right;
+		}
+		else{
+			return grandParent.left;
+		}
+	}
+	
+	private void checkPutCase1(Node<K, E> node){
+		// CASE 1: Make root Black if needed
+		if(node.key.compareTo(root.key) == 0){
 			root.color = true;
+			return;
 		}
-		
-		// Check if it's parent is red, thus double red problem
-		if(parentOf(putHeight - 1, addingNode) == false){
-			// Check if sibling is Black or null
-			if()
-			
+		else{
+			checkPutCase2(node);
 		}
+	}
 		
-			
-			
-				// If yes, then rotate, recolor, then check again
+	private void checkPutCase2(Node<K, E> node){
+		// CASE 2: Check if node and Parent are Red, and quit if not
+		if(node.parent.color == true || node.color == true){
+			return;
+		}
+		else{
+			checkPutCase3(node);
+		}
+	}
 		
-			// Else, recolor and check again
-		
-		// Else, then parent is black, tree is good, done
-		return false;
+	private void checkPutCase3(Node<K, E> node){
+		// CASE 3: Check if Parent and Uncle are Red, if so, turn Black and turn Grandparent Red
+		Node<K, E> uncle = getUncle(node);
+		if(uncle != null && uncle.color == false){
+			//System.out.println("here");
+			node.parent.color = true;
+			uncle.color = true;
+			Node<K, E> grandParent = getGrandParent(node);
+			grandParent.color = false;
+			checkPutCase1(grandParent);
+		}
+		else{
+			checkPutCase4(node);
+		}
 	}
 	
-	private boolean siblingOf(){
-		return false;
+	private void checkPutCase4(Node<K, E> node){
+		Node<K, E> grandParent = getGrandParent(node);
+		if(node == node.parent.right && node.parent == grandParent.left){
+			rotateLeft_Bent(node.parent);
+			node = node.left;
+		}
+		else if(node == node.parent.left && node.parent == grandParent.right){
+			rotateRight_Bent(node.parent);
+			node = node.right;
+		}
+		checkPutCase5(node);
 	}
 	
-	private boolean parentOf(int parentHeight, Node<K, E> addingNode){
-		Node<K, E> currentNode = root;
-		int height = 0;
-		while(true){
-			if(height == parentHeight){
-				break;
-			}
-			else if(addingNode.key.compareTo(currentNode.key) < 0){
-				currentNode = currentNode.left;
-				height++;
-			}
-			else if(addingNode.key.compareTo(currentNode.key) > 0){
-				currentNode = currentNode.right;
-				height++;
-			}
-			else{
-				System.out.println("Something went wrong when trying to find the parent");
-			}
+	private void checkPutCase5(Node<K, E> node){
+		Node<K, E> grandParent = getGrandParent(node);
+		node.parent.color = true;
+		grandParent.color = false;
+		//System.out.println(node.key);
+		if(node == node.parent.left){
+			rotateRight_Line(grandParent);
 		}
-		return currentNode.color;
+		else{
+			rotateLeft_Line(grandParent);
+		}
+		
+		//System.out.println(node.key + "  case 5");
+		if(node.parent == root){
+			root.color = true;
+			return;
+		}
+		else{
+			//node.parent.left.color = true;
+			//node.parent.right.color = true;
+			checkPutCase1(node.parent);
+		}
 	}
+	
+	private void rotateLeft_Line(Node<K, E> node){
+		Node<K, E> savedParent = node.parent;
+		Node<K, E> savedRightChild_Left = node.right.left;
+		Node<K, E> savedRightChild = node.right;
+		
+		savedRightChild.parent = savedParent;
+		if(savedParent != null){
+			savedParent.right = savedRightChild;
+			//savedParent.right = savedRightChild;
+		}
+		node.right = savedRightChild_Left;
+		if(savedRightChild_Left != null){
+			savedRightChild_Left.parent = node;
+		}
+		node.parent = savedRightChild;
+		savedRightChild.left = node;
+		if(savedRightChild.parent == null){
+			root = savedRightChild;
+		}
+	}
+	
+	private void rotateLeft_Bent(Node<K, E> node){
+		Node<K, E> savedParent = node.parent;
+		Node<K, E> savedRightChild_Left = node.right.left;
+		Node<K, E> savedRightChild = node.right;
+		
+		savedRightChild.parent = savedParent;
+		if(savedParent != null){
+			savedParent.left = savedRightChild;
+			//savedParent.right = savedRightChild;
+		}
+		node.right = savedRightChild_Left;
+		if(savedRightChild_Left != null){
+			savedRightChild_Left.parent = node;
+		}
+		node.parent = savedRightChild;
+		savedRightChild.left = node;
+		if(savedRightChild.parent == null){
+			root = savedRightChild;
+		}
+	}
+	
+	private void rotateRight_Line(Node<K, E> node){
+		Node<K, E> savedParent = node.parent;
+		Node<K, E> savedLeftChild_Right = node.left.right;
+		Node<K, E> savedLeftChild = node.left;
+		
+		savedLeftChild.parent = savedParent;
+		if(savedParent != null){
+			savedParent.left = savedLeftChild;
+		}
+		node.left = savedLeftChild_Right;
+		if(savedLeftChild_Right != null){
+			savedLeftChild_Right.parent = node;
+		}
+		node.parent = savedLeftChild;
+		savedLeftChild.right = node;
+		if(savedLeftChild.parent == null){
+			root = savedLeftChild;
+		}
+	}
+	
+	private void rotateRight_Bent(Node<K, E> node){
+		Node<K, E> savedParent = node.parent;
+		Node<K, E> savedLeftChild_Right = node.left.right;
+		Node<K, E> savedLeftChild = node.left;
+		
+		savedLeftChild.parent = savedParent;
+		if(savedParent != null){
+			savedParent.right = savedLeftChild;
+		}
+		node.left = savedLeftChild_Right;
+		if(savedLeftChild_Right != null){
+			savedLeftChild_Right.parent = node;
+		}
+		node.parent = savedLeftChild;
+		savedLeftChild.right = node;
+		if(savedLeftChild.parent == null){
+			root = savedLeftChild;
+		}
+	}
+	
 	
 	/**
 	 * Places the value in the tree as a Red value where it belongs
 	 * @param key
 	 * @param element
 	 */
-	private int placeInitialRedValue(K key, E element){
-		Node<K, E> addingNode = new Node<>(false, key, element);
+	private Node<K, E> placeInitialRedValue(K key, E element){
 		Node<K, E> currentNode = root;
-		int height = 0;
-		while(true){
-			if(currentNode == null){  // Root is setup using default constructor making it null
-				root = new Node<>(false, key, element);
-				size++;
-				break;
-			}
-			else if(key.compareTo(currentNode.key) < 0){
-				if(currentNode.left == null){
-					currentNode.left = addingNode;
-					size++;
-					height++;
-					break;
+		Node<K, E> addingNode = new Node<>(false, key, element);
+		
+		// Root is setup using default constructor making it null
+		if(root == null){  
+			root = addingNode;
+			root.parent = null;
+			size++;
+			return root;
+		}
+		else{
+			while(true){
+				if(key.compareTo(currentNode.key) < 0){
+					if(currentNode.left == null){
+						currentNode.left = addingNode;
+						addingNode.parent = currentNode;
+						size++;
+						return addingNode;
+					}
+					else{
+						currentNode = currentNode.left;
+					}
+				}
+				else if(key.compareTo(currentNode.key) > 0){
+					if(currentNode.right == null){
+						currentNode.right = addingNode;
+						addingNode.parent = currentNode;
+						size++;
+						return addingNode;
+					}
+					else{
+						currentNode = currentNode.right;
+					}
+				}
+				// No addition to graph, only change
+				else if(key.compareTo(currentNode.key) == 0){
+					currentNode.data = element;
+					return null;
 				}
 				else{
-					currentNode = currentNode.left;
-					height++;
+					System.out.println("Something went wrong when adding the node");
 				}
-			}
-			else if(key.compareTo(currentNode.key) > 0){
-				if(currentNode.right == null){
-					currentNode.right = addingNode;
-					size++;
-					height++;
-					break;
-				}
-				else{
-					currentNode = currentNode.right;
-					height++;
-				}
-			}
-			else if(key.compareTo(currentNode.key) == 0){
-				currentNode.data = element;
-				break;
-			}
-			else{
-				System.out.println("Something went wrong when adding the node");
 			}
 		}
-		return height;
 	}
 
 	@Override
 	public E get(K key) {
-		// TODO Auto-generated method stub
-		return null;
+		Node<K, E> currentNode = root;
+		while(true){
+			if(currentNode == null){  // Root is setup using default constructor making it null
+				throw new NoSuchElementException();
+			}
+			if(key.compareTo(currentNode.key) < 0){
+				if(currentNode.left == null){
+					throw new NoSuchElementException();
+				}
+				else{
+					currentNode = currentNode.left;
+				}
+			}
+			else if(key.compareTo(currentNode.key) > 0){
+				if(currentNode.right == null){
+					throw new NoSuchElementException();
+				}
+				else{
+					currentNode = currentNode.right;
+				}
+			}
+			else if(key.compareTo(currentNode.key) == 0){
+				return currentNode.data;
+			}
+			else{
+				System.out.println("Something went wrong when getting the node");
+			}
+		}
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return size;
 	}
 
 	@Override
 	public int getHeight() {
-		// TODO Auto-generated method stub
-		return 0;
+		if(root == null){
+			return 0;
+		}
+		// Subtract one as it includes the root in the height
+		return getRecursiveHeight(root);
+	}
+	
+	/**
+	 * Returns this map in string form, surrounded by [] brackets and with each
+     * items separated by a comma and space. each key element pair should be
+     * written as key: element.  For example:
+     * [1: Dog, 2: Cat, 3: Bird, 4: Cow]
+     * the order of pairs should be in ascending key order.
+     * @return String representing the tree formated to look similar to an array
+     * Ex. [1: Dog, 2: Cat, 3: Bird, 4: Cow]
+	 */
+	@Override
+	public String toString(){
+		StringBuilder s = new StringBuilder();
+		
+		if(root == null){
+			return "[]";
+		}
+		else{
+			s = (inOrder(root, s));
+			s.delete(s.length() - 2, s.length());
+			s.insert(0, "[");
+			s.append("]");
+			
+			String result = s.toString();
+			return result;
+		}
+	}
+	
+	/**
+	 * 
+	 * @param root
+	 * @param s
+	 * @return
+	 */
+	private StringBuilder inOrder(Node<K, E> root, StringBuilder s){
+		if(root.left != null){
+			inOrder(root.left, s);
+		}
+		
+		s.append(root.key + ": " + root.data + ", ");
+		
+		if(root.right != null){
+			inOrder(root.right, s);
+		}
+		return s;
+	}
+	
+	/**
+	 * 
+	 * @param root
+	 * @return
+	 */
+	private int getRecursiveHeight(Node<K, E> node){
+		// Terminates branch and sends it back up
+		if(node == null || node.left == null || node.right == null){
+			return 0;
+		}
+		
+		int left = getRecursiveHeight(node.left);
+		int right = getRecursiveHeight(node.right);
+		
+		if(left > right){
+			return left + 1;
+		}
+		else{
+			return right + 1;
+		}
+	}
+	
+	
+	public int getMinimumHeight() {
+		if(root == null){
+			return 0;
+		}
+		// Subtract one as it includes the root in the height
+		return this.getRecursiveMinHeight(root) - 1;
+	}
+	
+	private int getRecursiveMinHeight(Node<K, E> node){
+		// Terminates branch and sends it back up
+		if(node == null){
+			return 0;
+		}
+		
+		int left = getRecursiveHeight(node.left);
+		int right = getRecursiveHeight(node.right);
+		
+		if(left < right){
+			return left + 1;
+		}
+		else{
+			return right + 1;
+		}
 	}
 
 	@Override
 	public String getTreeString() {
-		// TODO Auto-generated method stub
-		return null;
+		if(root == null){
+			return "[]";
+		}
+		StringBuilder s = new StringBuilder();
+		s = treeStringBuilder(root, s, 0, true);
+		s.delete(s.length() - 2, s.length());
+		
+		if(getHeight() > 0){
+			//s.append("]");
+			//s.append("]");
+		}
+		String result = s.toString();
+		return result;
+	}
+	
+	/**
+	 * 
+	 * @param root
+	 * @param s
+	 * @return
+	 */
+	public StringBuilder treeStringBuilder(Node<K, E> root, StringBuilder s, int curHeight, boolean curTreeSide){
+		
+		
+		s.append("[" + boolToColor(root.color) + ", " + root.key + ", ");
+		
+		if(root.left != null){
+			treeStringBuilder(root.left, s, curHeight++, true);
+		}
+		else{
+			s.append("null, ");
+		}
+		
+		if(root.right != null){
+			treeStringBuilder(root.right, s, curHeight++, false);
+		}
+		else{
+			s.append("null");
+			// All nodes to the left side of the tree only need one bracket
+			if(curTreeSide == true){
+				s.append("]");
+			}
+			// If a node to the right side has a child to the left, but not right, adjust ] count
+			else if(root.left != null){
+				for(int ii = 0; ii <= curHeight - 1; ii++){
+					s.append("]");
+				}
+			}
+			
+			else if(getHeight() - getMinimumHeight() == 0){
+				for(int ii = 0; ii <= getHeight() - 3; ii++){
+					s.append("]");
+				}
+				s.append("]");
+			}
+			else if(getHeight() - getMinimumHeight() == 1){
+				for(int ii = 0; ii <= getHeight() - 3; ii++){
+					s.append("]");
+				}
+			}
+			
+			// Put in as many ] as height of tree, minus one cause left side, right most node doesn't need
+			// final ] to close it in with will right side, right most node gets it in getTreeString()
+			else{
+				
+					for(int ii = 0; ii <= getHeight() - getMinimumHeight(); ii++){
+						s.append("]");
+					}
+					//s.append("]");
+			}
+			s.append(", ");		
+		}
+		return s;
+	}
+	
+	private String boolToColor(boolean color){
+		if(color == false){
+			return "Red";
+		}
+		else{
+			return "Black";
+		}
 	}
 	
 	/**
 	 * Node class for use by Binary Search Tree
 	 */
 	public class Node<Key, D>{
-		public Node<Key, D> left;
-		public Node<Key, D> right;
+		public Node<Key, D> left = null;
+		public Node<Key, D> right = null;
+		public Node<Key, D> parent = null;
 		public Key key;
 		public D data;
 		public boolean color;
--- Project 4/tempTest.java
+++ Project 4/tempTest.java
@@ -1,7 +1,7 @@
 
 public class tempTest {
 	public static void main(String[] args){
-		BinarySearchTree<Integer,String> tree = new BinarySearchTree<Integer,String>();
+		RedBlackTree<Integer,String> tree = new RedBlackTree<Integer,String>();
 		BinarySearchTree<Integer,String> tree2 = new BinarySearchTree<Integer,String>();
 		
 		/*
@@ -10,19 +10,72 @@ public class tempTest {
 	    tree.put(3, "Three");
 	    */
 		
+		tree.put(1, "One");
+		tree.put(2, "Two");
+		tree.put(3, "Three");
+		tree.put(4, "Four");
+		tree.put(5, "Five");
+		tree.put(6, "Six");
+		//tree.put(7, "Seven");
+		//tree.put(8, "Eight");
+		//tree.put(9, "Nine");
+		/*
+		tree.put(6, "Six");
+		tree.put(5, "Five");
+		tree.put(4, "Four");
+		tree.put(3, "Three");
+		tree.put(2, "Two");
+		tree.put(1, "One");
+		tree.put(13, "Thirteen");
+		tree.put(11,"Eleven");
+		tree.put(10,"Ten");
+		tree.put(9, "Nine");
+		tree.put(8, "Eight");
+		tree.put(7, "Seven");
 		
-	    tree.put(1, "One");
-	    tree.put(2, "Two");
+		tree.put(6, "Six");
+		tree.put(2, "Two");
+		tree.put(13, "Thirteen");
+		tree.put(9, "Nine");
+		tree.put(10,"Ten");
+		*/
+		/*
+		tree.put(8, "Eight");
 	    tree.put(3, "Three");
+	    tree.put(10, "Ten");
+	    tree.put(1, "One");
+	    tree.put(6, "Six");
+	    tree.put(14, "Fourteen");
 	    tree.put(4, "Four");
-	    tree.put(5, "Five");
+	    tree.put(7, "Seven");
+	   // tree.put(13, "Thirteen");
+	    * 
+	    */
+		
+		//System.out.println(tree.getHeight());
+		//System.out.println(tree.getMinimumHeight());
+		//System.out.println(tree.getHeight()- tree.getMinimumHeight());
+		/*
+		tree.put(3, "Three");
+		tree.put(1, "One");
+		tree.put(2, "Two");
+	    */
+		//tree.put(5, "Five");
+		//tree.put(7, "Seven");
+		//tree.put(9, "Nine");
+		//tree.put(8, "Eight");
+		//tree.put(10,"Ten");
+		//tree.put(11,"Eleven");
+		
+	    //tree.put(4, "Four");
+	    /* tree.put(5, "Five");
 	    tree.put(6, "Six");
 	    tree.put(7, "Seven");
 	    tree.put(8, "Eight");
-	    tree.put(9, "Nine");
+	    tree.put(9, "Nine");*/
 	    
 	    
-	    
+	    /*
 		tree2.put(8, "Eight");
 	    tree2.put(3, "Three");
 	    tree2.put(10, "Ten");
@@ -33,8 +86,8 @@ public class tempTest {
 	    tree2.put(7, "Seven");
 	    tree2.put(13, "Thirteen");
 	    //tree2.put(12, "Twelve");
-	    
+	    */
 	    System.out.println(tree.getTreeString());
-	    System.out.println(tree2.getTreeString());
+	    //System.out.println(tree2.getTreeString());
 	}
 }
